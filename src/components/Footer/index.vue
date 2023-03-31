93ce04ec74a6f5cc137325aff2ceeb7acc5f35d0
xukai <xukai@co-mall.com>
2023-03-31 09:44:17 +0800

    set info of self

--- src/components/Footer/index.vue
+++ src/components/Footer/index.vue
@@ -3,7 +3,7 @@
     <div class="power" v-show="!store.playerState">
       <span
         >Copyright&nbsp;&copy;&nbsp;{{ fullYear }}
-        <a href="https://imsyy.top">無名</a>
+        <a href="https://imsyy.top">Suyunyixi</a>
       </span>
       <!-- 以下信息请不要修改哦 -->
       <span class="hidden"
@@ -16,7 +16,7 @@
       >&nbsp;&amp;
       <!-- 站点备案 -->
       <a href="https://beian.miit.gov.cn" target="_blank"
-        >豫ICP备2022018134号-1</a
+        >鄂ICP备2022002629号-1</a
       >
     </div>
     <div class="lrc" v-show="store.playerState">
